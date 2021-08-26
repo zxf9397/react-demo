f44ca45bcda956e918071b1cad05969eceee8bff
Mubens <zhuxiaofengzj@qq.com>
2021-08-26 15:15:24 +0800

    feat: clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react';
 import 'antd/dist/antd.css';
-import EditableTable from './components/EditTable/EditTable';
-import ScrollLoad from './components/ScrollLoad/ScrollLoad';
-import Mytable from './components/EditTable/Mytable';
 import FabricCanvas from './components/FabricCanvas/FabricCanvas';
-// import Scrolling from './pages/Scrolling/Scrolling';
-// import VirtualScroll from './pages/VirtualScroll/VirtualScroll';
 
 function App() {
-  const [list, setList] = useState<number[]>([]);
-  useEffect(() => {
-    let list = [];
-    for (let i = 0; i < 100; i++) {
-      list.push(i);
-    }
-    setList(list);
-  }, []);
   return (
     <div className="App">
-      {/* <EditableTable /> */}
       <FabricCanvas />
-      {/* <Mytable /> */}
     </div>
   );
 }
