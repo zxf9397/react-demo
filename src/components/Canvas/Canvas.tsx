40f4cc23e7a5e14d4d6198fa6f2d317c34ed2389
Mubens <zhuxiaofengzj@qq.com>
2021-11-05 18:44:50 +0800

    feat: update

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,6 +1,7 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { Input } from 'antd';
+import React, { useCallback, useEffect, useState } from 'react';
 import './Canvas.scss';
-import DesignTool, { Design } from './utils/design.class';
+import DesignTool, { Design, InterfacePayload } from './utils/design.class';
 
 const imageUrl = 'https://clkj-dev.oss-cn-qingdao.aliyuncs.com/images/material/short/472-04c45fe3eb8243033b49518c55f87824.jpg?x-oss-process=image';
 const options = {
@@ -17,31 +18,53 @@ interface CanvasCardProps {
 }
 
 function CanvasCard({ design, designer, onDelete }: CanvasCardProps) {
-  // const ref = useRef<HTMLInputElement>(null);
-  const [value, setValue] = useState('');
+  const [width, setWidth] = useState('');
+  const [height, setHeight] = useState('');
+  const [angle, setAngle] = useState('');
 
   useEffect(() => {
-    // if (ref.current) {
-    // ref.current.value = design.getScaledWidth().toFixed(0);
-    setValue(design.getScaledWidth().toFixed(0));
-    // }
+    setWidth(design.getScaledWidth().toFixed(0));
+    setHeight(design.getScaledHeight().toFixed(0));
+    setAngle(design.angle.toFixed(0));
   }, [design]);
+
+  useEffect(() => {
+    function change(payload: InterfacePayload) {
+      setWidth((payload.width * payload.scaleX).toFixed(0));
+      setHeight((payload.height * payload.scaleY).toFixed(0));
+      setAngle(payload.angle.toFixed(0));
+    }
+    design.on('datachange', change);
+    return () => {
+      design.off('datachange', change);
+    };
+  }, []);
+  const setDesign = useCallback(
+    function <T extends keyof InterfacePayload>(type: T, handler: (value: string) => void) {
+      return (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        if (typeof design[type] === 'number') {
+          handler(value);
+          designer?.set(design, type, Number(value));
+        }
+      };
+    },
+    [design]
+  );
   return (
     <div className="coo-canvas__card">
-      <input
-        // ref={ref}
-        value={value}
-        type="range"
-        min={0}
-        max={400}
-        onChange={(e) => {
-          const width = e.target.value;
-          setValue(width);
-          // ref.current && (ref.current.value = width);
-          design.scaleToWidth(+width);
-          designer?.canvas.renderAll();
-        }}
-      />
+      <div>
+        <span>width:</span>
+        <Input value={width} onChange={setDesign('width', setWidth)} />
+      </div>
+      <div>
+        <span>height:</span>
+        <Input value={height} onChange={setDesign('height', setHeight)} />
+      </div>
+      <div>
+        <span>angle:</span>
+        <Input value={angle} onChange={setDesign('angle', setAngle)} />
+      </div>
       <button onClick={onDelete}>删除</button>
     </div>
   );
@@ -72,10 +95,11 @@ export default function Canvas() {
         <canvas id="canvas" width="1200" height="600"></canvas>
       </div>
       <div className="coo-canvas__buttons">
-        <button onClick={() => addImage(imageUrl, options)}>添加图片</button>
+        {/* <button onClick={() => addImage(imageUrl, options)}>添加图片</button>
         <button onClick={designer?.cropEnv.enterCropping}>开始裁切</button>
         <button onClick={designer?.cropEnv.cancelCropping}>取消裁切</button>
-        <button onClick={designer?.cropEnv.confirmCropping}>确认裁切</button>
+        <button onClick={designer?.cropEnv.confirmCropping}>确认裁切</button> */}
+        <button onClick={designer?.addPath}>addPath</button>
       </div>
       <div className="coo-canvas__cards">
         {designs.map((design, i) => (
--- src/components/Canvas/utils/design.class.ts
+++ src/components/Canvas/utils/design.class.ts
@@ -1,7 +1,19 @@
 import { fabric } from 'fabric';
 import CropEnvironment from './crop.class';
 
-export type Design = Required<fabric.Object> & {};
+export interface InterfacePayload {
+  width: number;
+  height: number;
+  scaleX: number;
+  scaleY: number;
+  angle: number;
+}
+interface CustomParameters {
+  on(eventName: 'datachange', handler: (e: InterfacePayload) => void): void;
+  off(eventName: 'datachange', handler: (e: InterfacePayload) => void): void;
+}
+
+export type Design = Required<fabric.Object> & CustomParameters;
 type TypeUpdateLayerDesigns = (designs: Design[]) => void;
 
 interface DesignToolOptions {
@@ -35,8 +47,8 @@ export default class DesignTool {
           this.canvas.discardActiveObject();
           this.canvas.setActiveObject(image);
           this.canvas.add(image).renderAll();
-          this.updateLayerDesigns([...this.layerDesigns, image as Design]);
-          resolve(image as Design);
+          this.updateLayerDesigns([...this.layerDesigns, image as unknown as Design]);
+          resolve(image as unknown as Design);
         },
         {
           crossOrigin: 'anonymous',
@@ -57,35 +69,85 @@ export default class DesignTool {
     this.updateLayerDesigns([...this.layerDesigns]);
   };
 
-  flip = (type: 'flipX' | 'flipY') => {
-    const activeObj = this.canvas.getActiveObject();
-    if (!activeObj || this.cropEnv.cropping) {
-      return;
-    }
-    const flipX = !activeObj.get(type);
-    activeObj.set(type, flipX).setCoords();
-    // setCoords for activeSelection._objects
-    (activeObj as any)?.addWithUpdate?.();
+  scaleToWidth = (design: Design, width: number) => {
+    design.scaleToWidth(width);
     this.canvas.renderAll();
-    this.canvas.fire('object:modified', { target: activeObj });
   };
 
-  private scaleObject = (e: fabric.IEvent) => {
-    const target = (e.transform as any).target;
+  addPath = () => {
+    const pathArray =
+      'M78.2,0.1c0,0,9.4,79.1,2.3,117  c-4.5,24.1-31.8,106.2-56.3,108.7c-12.7,1.3-24.2-11.9-16.5-15.5C15,207,40.2,231.1,19,261.7c-9.8,14.1-24.7,31.9-12.5,44.9  c11.3,12,53-36.8,59.2-23.8c8.6,18-40.8,23-28,49.2c14.7,30.4,21.6,39.9,48,58.5c31.3,22,147,66.2,147.2,149.5';
+    const path = new fabric.Path(pathArray, {
+      fill: '',
+      stroke: 'black',
+      strokeDashArray: [5, 5],
+      strokeDashOffset: 0,
+    });
+    this.canvas.add(path);
+
+    const animatePath = (path: fabric.Path) => {
+      path.animate('strokeDashOffset', '-=3', {
+        duration: 100,
+        onChange: this.canvas.renderAll.bind(this.canvas),
+        onComplete: function () {
+          animatePath(path);
+        },
+      });
+    };
+    console.log(path);
+    animatePath(path);
+  };
+
+  set = <K extends keyof InterfacePayload>(design: Design, key: K, value: Design[K]) => {
+    switch (key) {
+      case 'angle':
+        design.rotate(value);
+        break;
+      case 'width':
+        design.set('scaleX', value / design.width);
+        break;
+      case 'height':
+        design.set('scaleY', value / design.height);
+        break;
+      default:
+        design.set(key, value);
+    }
+    this.canvas.renderAll();
+  };
+
+  flip = (type: 'flipX' | 'flipY') => {
+    const active = this.canvas.getActiveObject();
+    if (!active || this.cropEnv.cropping) {
+      return;
+    }
+    const flipX = !active.get(type);
+    active.set(type, flipX).setCoords();
+    (active as any)?.addWithUpdate?.();
+    this.canvas.renderAll();
+    this.canvas.fire('object:modified', { target: active });
+  };
+
+  private datachange = (e: fabric.IEvent) => {
+    const target = ((e.transform as any) || e).target;
     if (target._objects) {
-      //
+      target._objects.forEach((design: Design) => {
+        const { width, height } = design;
+        const { scaleX, scaleY, angle } = fabric.util.qrDecompose(design.calcTransformMatrix());
+        design.fire('datachange', { width, height, scaleX: Math.abs(scaleX), scaleY: Math.abs(scaleY), angle: (360 + angle) % 360 });
+      });
     } else {
-      const index = this.canvas.getObjects().findIndex((obj) => obj === target);
-      this.layerDesigns[index] = fabric.util.object.clone(target);
-      this.updateLayerDesigns([...this.layerDesigns]);
+      const { width, height, scaleX, scaleY, angle } = target;
+      target.fire('datachange', { width, height, scaleX, scaleY, angle });
     }
   };
 
   private bind() {
-    this.canvas.on('object:scaling', this.scaleObject);
+    this.canvas.on('object:scaling', this.datachange);
+    this.canvas.on('object:rotating', this.datachange);
   }
 
   unbind() {
-    this.canvas.off('object:scaling', this.scaleObject);
+    this.canvas.off('object:scaling', this.datachange);
+    this.canvas.off('object:rotating', this.datachange);
   }
 }
